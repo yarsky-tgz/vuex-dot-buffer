ec42bbf1d014d34c60c1a42e8019f6d2c90d907e
Yaroslav Dobzhanskij <merlyn.tgz@gmail.com>
2018-06-13 20:43:24 +0000

    checking for undefined

--- dist/vuex-dot-buffer.js
+++ dist/vuex-dot-buffer.js
@@ -6,7 +6,36 @@
 
   /**
    * Creates buffer plugin for vuex-dot with given trigger value used for buffer flush
+   * Second argument, cacheBump, is used to avoid computed property caching on buffered changes.
+   *
+   * In Vue computed properties are dependent from real reactive properties of your state (data(), or vuex state).
+   *
+   * So you need to add such property, your buffered computed properties to be dependent on, and pass it's name
+   * as second argument.
+   *
+   * for example:
+   *
+   * ```javascript
+   * export default {
+   *   data() {
+   *     return {
+   *       $$bufferCacheBump: 0
+   *     }
+   *   },
+   *   computed: {
+   *     ...takeState('user')
+   *       .compose(['name', 'email'])
+   *       .use(buffer('version', '$$bufferCacheBump'))
+   *       .commit('EDIT_USER')
+   *       .map();
+   *   }
+   * }
+   * ```
+   *
+   * **Notice**: your
+   *
    * @param trigger
+   * @param cacheBump name of your `vm` property, that shall be used for vue computed cache updating
    * @return {*}
    */
   var vuexDotBuffer = (trigger, cacheBump) => {
@@ -22,12 +51,15 @@
       getter(key, nextGetter) {
         if (cacheBump) this[cacheBump]; // so, vue shall notice, that your cache bumping property is accessed during
                                         // computed calculation, and it shall mark it as dependent on it too.
-        if (buffer[ key ]) return buffer[ key ];
+        if (typeof buffer[ key ] !== 'undefined') return buffer[ key ];
         else return nextGetter();
       },
       inject: {
         [ trigger ]: {
-          get: () => triggerValue,
+          get() {
+            if (cacheBump) this[cacheBump];
+            return triggerValue;
+          },
           set(value) {
             triggerValue = value;
             for (let setter of Object.keys(delayed)) delayed[ setter ](buffer[ setter ]);
--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 /**
  * Creates buffer plugin for vuex-dot with given trigger value used for buffer flush
+ * Second argument, cacheBump, is used to avoid computed property caching on buffered changes.
+ *
+ * In Vue computed properties are dependent from real reactive properties of your state (data(), or vuex state).
+ *
+ * So you need to add such property, your buffered computed properties to be dependent on, and pass it's name
+ * as second argument.
+ *
+ * for example:
+ *
+ * ```javascript
+ * export default {
+ *   data() {
+ *     return {
+ *       $$bufferCacheBump: 0
+ *     }
+ *   },
+ *   computed: {
+ *     ...takeState('user')
+ *       .compose(['name', 'email'])
+ *       .use(buffer('version', '$$bufferCacheBump'))
+ *       .commit('EDIT_USER')
+ *       .map();
+ *   }
+ * }
+ * ```
+ *
+ * **Notice**: your
+ *
  * @param trigger
+ * @param cacheBump name of your `vm` property, that shall be used for vue computed cache updating
  * @return {*}
  */
 module.exports = (trigger, cacheBump) => {
@@ -16,12 +45,15 @@ module.exports = (trigger, cacheBump) => {
     getter(key, nextGetter) {
       if (cacheBump) this[cacheBump]; // so, vue shall notice, that your cache bumping property is accessed during
                                       // computed calculation, and it shall mark it as dependent on it too.
-      if (buffer[ key ]) return buffer[ key ];
+      if (typeof buffer[ key ] !== 'undefined') return buffer[ key ];
       else return nextGetter();
     },
     inject: {
       [ trigger ]: {
-        get: () => triggerValue,
+        get() {
+          if (cacheBump) this[cacheBump];
+          return triggerValue;
+        },
         set(value) {
           triggerValue = value;
           for (let setter of Object.keys(delayed)) delayed[ setter ](buffer[ setter ]);
